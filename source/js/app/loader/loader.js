eeee180fb079a66c44483ef69a94c4488cc0df77
Illia Olenchenko <icrosil@gmail.com>
2016-03-31 00:57:03 +0300

    A bit of fixes
    All scheme moved to Angular.
    Moving graphs from ThreeBox to D3js.
    Added editorconfig, feel free to add specifics based on existing.

---
+++ source/js/app/loader/loader.js
@@ -0,0 +1,18 @@
+/**
+ * authors - JYRIK (Julia, Yana, Rostik, Illia, Katya)
+ */
+
+
+(function() {
+  'use strict';
+
+  angular
+    .module('solver')
+    .controller('SolverLoaderController', SolverLoaderController);
+
+  SolverLoaderController.$inject = [];
+  function SolverLoaderController() {
+    var vm = this;
+  }
+
+})();
---
+++ source/js/app/solver.controller.js
@@ -0,0 +1,28 @@
+/**
+ * authors - JYRIK (Julia, Yana, Rostik, Illia, Katya)
+ */
+
+
+(function() {
+  'use strict';
+
+  angular
+    .module('solver')
+    .controller('SolverController', SolverController);
+
+  SolverController.$inject = ['$location'];
+  function SolverController($location) {
+    var vm = this;
+    vm.pageClass = pageClass;
+
+    function pageClass() {
+      switch ($location.path()) {
+        case '/data':
+          return 'blue-page';
+        default:
+          return '';
+      }
+    }
+  }
+
+})();
